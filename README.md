9f4f30656322e894e848da4858c29182c3eb35a8
James R. Grant <JamesRossGrant@gmail.com>
2025-12-02 06:28:04 -0600

    Clean up input files

---
+++ README.md
@@ -0,0 +1,3 @@
+Solutions for Advent of Code 2025 in Rust.
+
+See [www.adventofcode.com](https://www.adventofcode.com)
--- input/setup.py
+++ input/setup.py
@@ -1,4 +1,4 @@
-for i in range(1, 26):
+for i in range(1, 13):
     f = open(f"{i:02}" + ".txt", "x")
     f.close()
     f = open(f"{i:02}" + "_test.txt", "x")
